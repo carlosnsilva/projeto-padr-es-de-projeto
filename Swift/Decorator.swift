30cddfb48d7ce17f4cb6937c734f4883432a2600
carlosnsilva <cmanoel17@gmail.com>
2021-10-01 00:15:54 -0300

    Subindo ajustes

--- Swift/Decorator.swift
+++ Swift/Decorator.swift
@@ -1,63 +1,198 @@
+import UIKit
 import XCTest
 
 
-protocol Component {
+protocol ImageEditor: CustomStringConvertible {
 
-    func operation() -> String
+    func apply() -> UIImage
 }
 
-class ConcreteComponent: Component {
+class ImageDecorator: ImageEditor {
 
-    func operation() -> String {
-        return "ConcreteComponent"
+    private var editor: ImageEditor
+
+    required init(_ editor: ImageEditor) {
+        self.editor = editor
+    }
+
+    func apply() -> UIImage {
+        print(editor.description + " applies changes")
+        return editor.apply()
+    }
+
+    var description: String {
+        return "ImageDecorator"
     }
 }
 
-class Decorator: Component {
+extension UIImage: ImageEditor {
 
-    private var component: Component
-
-    init(_ component: Component) {
-        self.component = component
+    func apply() -> UIImage {
+        return self
     }
 
-    func operation() -> String {
-        return component.operation()
+    open override var description: String {
+        return "Image"
     }
 }
 
-class ConcreteDecoratorA: Decorator {
 
-    override func operation() -> String {
-        return "ConcreteDecoratorA(" + super.operation() + ")"
+
+class BaseFilter: ImageDecorator {
+
+    fileprivate var filter: CIFilter?
+
+    init(editor: ImageEditor, filterName: String) {
+        self.filter = CIFilter(name: filterName)
+        super.init(editor)
+    }
+
+    required init(_ editor: ImageEditor) {
+        super.init(editor)
+    }
+
+    override func apply() -> UIImage {
+
+        let image = super.apply()
+        let context = CIContext(options: nil)
+
+        filter?.setValue(CIImage(image: image), forKey: kCIInputImageKey)
+
+        guard let output = filter?.outputImage else { return image }
+        guard let coreImage = context.createCGImage(output, from: output.extent) else {
+            return image
+        }
+        return UIImage(cgImage: coreImage)
+    }
+
+    override var description: String {
+        return "BaseFilter"
     }
 }
 
-class ConcreteDecoratorB: Decorator {
+class BlurFilter: BaseFilter {
 
-    override func operation() -> String {
-        return "ConcreteDecoratorB(" + super.operation() + ")"
+    required init(_ editor: ImageEditor) {
+        super.init(editor: editor, filterName: "CIGaussianBlur")
+    }
+
+    func update(radius: Double) {
+        filter?.setValue(radius, forKey: "inputRadius")
+    }
+
+    override var description: String {
+        return "BlurFilter"
     }
 }
 
-class Client {
+class ColorFilter: BaseFilter {
 
-    static func someClientCode(component: Component) {
-        print("Result: " + component.operation())
+    required init(_ editor: ImageEditor) {
+        super.init(editor: editor, filterName: "CIColorControls")
     }
 
+    func update(saturation: Double) {
+        filter?.setValue(saturation, forKey: "inputSaturation")
+    }
+
+    func update(brightness: Double) {
+        filter?.setValue(brightness, forKey: "inputBrightness")
+    }
+
+    func update(contrast: Double) {
+        filter?.setValue(contrast, forKey: "inputContrast")
+    }
+
+    override var description: String {
+        return "ColorFilter"
+    }
 }
 
-class DecoratorConceptual: XCTestCase {
+class Resizer: ImageDecorator {
 
-    func testDecoratorConceptual() {
+    private var xScale: CGFloat = 0
+    private var yScale: CGFloat = 0
+    private var hasAlpha = false
 
-        print("Client: I've got a simple component")
-        let simple = ConcreteComponent()
-        Client.someClientCode(component: simple)
-        let decorator1 = ConcreteDecoratorA(simple)
-        let decorator2 = ConcreteDecoratorB(decorator1)
-        print("\nClient: Now I've got a decorated component")
-        Client.someClientCode(component: decorator2)
+    convenience init(_ editor: ImageEditor, xScale: CGFloat = 0, yScale: CGFloat = 0, hasAlpha: Bool = false) {
+        self.init(editor)
+        self.xScale = xScale
+        self.yScale = yScale
+        self.hasAlpha = hasAlpha
+    }
+
+    required init(_ editor: ImageEditor) {
+        super.init(editor)
+    }
+
+    override func apply() -> UIImage {
+
+        let image = super.apply()
+
+        let size = image.size.applying(CGAffineTransform(scaleX: xScale, y: yScale))
+
+        UIGraphicsBeginImageContextWithOptions(size, !hasAlpha, UIScreen.main.scale)
+        image.draw(in: CGRect(origin: .zero, size: size))
+
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return scaledImage ?? image
+    }
+
+    override var description: String {
+        return "Resizer"
+    }
+}
+
+
+class DecoratorRealWorld: XCTestCase {
+
+    func testDecoratorRealWorld() {
+
+        let image = loadImage()
+
+        print("Client: set up an editors stack")
+        let resizer = Resizer(image, xScale: 0.2, yScale: 0.2)
+
+        let blurFilter = BlurFilter(resizer)
+        blurFilter.update(radius: 2)
+
+        let colorFilter = ColorFilter(blurFilter)
+        colorFilter.update(contrast: 0.53)
+        colorFilter.update(brightness: 0.12)
+        colorFilter.update(saturation: 4)
+
+        clientCode(editor: colorFilter)
+    }
+
+    func clientCode(editor: ImageEditor) {
+        let image = editor.apply()
+        /// Note. You can stop an execution in Xcode to see an image preview.
+        print("Client: all changes have been applied for \(image)")
+    }
+}
+
+private extension DecoratorRealWorld {
+
+    func loadImage() -> UIImage {
+
+        let urlString = "https:// refactoring.guru/images/content-public/logos/logo-new-3x.png"
+
+        /// Note:
+        /// Do not download images the following way in a production code.
+
+        guard let url = URL(string: urlString) else {
+            fatalError("Please enter a valid URL")
+        }
+
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Cannot load an image")
+        }
+
+        guard let image = UIImage(data: data) else {
+            fatalError("Cannot create an image from data")
+        }
+        return image
     }
 }
